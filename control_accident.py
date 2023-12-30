cf715e5cfc98ebf44b98dfd58219cc0a888ca818
anonymousCoder29 <155138234+anonymousCoder29@users.noreply.github.com>
2023-12-30 03:14:30 -0500

    Update control_accident.py

--- control_accident.py
+++ control_accident.py
@@ -15,8 +15,7 @@ random.seed(5)
 #from scipy.optimize import odeint
 from main_OCBF import second_order_model
 
-from metavariable import s1, s2, s3, dt, noise1, noise2, const1, const2, const3, const4
-
+from metavariable_accident import s1, s2, s3, dt, noise1, noise2, const1, const2, const3, const4, max_range,L_end,trust_threshold
 def update_table(car):
     size = len(car["order"])
     order = car["order"]
