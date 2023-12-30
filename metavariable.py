7a66be8afd948ade3e17885731f4a9f00a5aedbc
anonymousCoder29 <155138234+anonymousCoder29@users.noreply.github.com>
2023-12-30 00:42:24 -0500

    Update metavariable.py

--- metavariable.py
+++ metavariable.py
@@ -1,6 +1,6 @@
-
-s1 = 1
-s2 = 1
+max_range = 450
+s1 = 0.5
+s2 = 0.5
 s3 = 0.5
 dt = 0.1
 noise1 = 0
@@ -9,6 +9,5 @@ const1 = 0
 const2 = 0
 const3 = 0
 const4 = 0
-max_range = 450
 L_end = 75
 trust_threshold = {'low': 0.3, 'high': 0.8}
